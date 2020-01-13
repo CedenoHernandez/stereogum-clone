1b1a91ea5b6a0c4a108d4b2b9aa490ab2f58289d
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-12 19:05:21 -0800

    Set up Footer component

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -7,7 +7,7 @@ class Featured extends Component {
         <button type="button" className="scroll"><i className="fas fa-chevron-left"></i></button>
         <img src="../images/Lost-In-Yesterday.jpg" alt=""/>
         <img src="../images/Moz-Not-A-Dog.jpg" alt=""/>
-        <img src="../images/Donna-Summer.jpg" alt=""/>
+        <img src="../images/Miss-Anthropocene.jpg" alt=""/>
         <img src="../images/abbey-road.jpg" alt=""/>
         <button type="button" className="scroll"><i className="fas fa-chevron-right"></i></button>
       </div>
