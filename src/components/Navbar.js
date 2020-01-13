adbd95ccb0fb3cb2b83c7c293556cdb99822137c
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-12 16:52:16 -0800

    Finish styling of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,21 +7,22 @@ class Navbar extends Component {
       <div className="navbar">
         <div>
           <img src="../images/stereogumlogo.jpg" className="logo" alt=""/>
-          <a href="#" className="siteTitle">Stereogum</a>
+          <a href="#" id="siteTitle">Stereogum</a>
         </div>
-        <div>
+        <div className="pageLinks">
+          <a href="#">Columns <i className="fas fa-caret-down"></i></a>
           <a href="#">New Music</a>
           <a href="#">Lists</a>
           <a href="#">Videos</a>
         </div>  
         <div className="siteLogos">
-          <i className="fab fa-facebook-f"></i>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-instagram"></i>
-          <i className="fab fa-spotify"></i>
+          <i className="fab fa-facebook-f fa-lg"></i>
+          <i className="fab fa-twitter fa-lg"></i>
+          <i className="fab fa-instagram fa-lg"></i>
+          <i className="fab fa-spotify fa-lg"></i>
         </div>
         <div className="search">
-          <i className="fas fa-search"></i>
+          <i className="fas fa-search fa-lg"></i>
         </div>
       </div>
     )
