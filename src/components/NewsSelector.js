dbb523e940c01ed1ccad98517a256356f4646662
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-15 14:54:51 -0800

    Add more site images

--- src/components/NewsSelector.js
+++ src/components/NewsSelector.js
@@ -12,7 +12,7 @@ class NewsSelector extends Component {
           <a href="#" id="most-commented">MOST COMMENTED</a>
         </div>
 
-
+        {/* Most Viewed */}
         <div className="pop-story">
           <img src="../images/BlackMidi.jpg" alt=""/>
           <div className="story-num">01</div>
@@ -60,6 +60,58 @@ class NewsSelector extends Component {
           <div className="story-num">08</div>
           <a href="#">Randy Newman, Elton John Up For Best Original Song Oscars</a>
         </div>
+
+        {/*Most Commented*/}
+        {/*
+        <div className="pop-story">
+          <img src="../images/Donna-Summer.jpg" alt=""/>
+          <div className="story-num">01</div>
+          <a href="#">The Number Ones: Donna Summer's "MacArthur Park" 513<i className="far fa-comment"></i></a>
+          
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/Anne-Murray.jpg" alt=""/>
+          <div className="story-num">02</div>
+          <a href="#">The Number Ones: Anne Murray's "You Needed Me" 480<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/Babs-Diamond.jpg" alt=""/>
+          <div className="story-num">03</div>
+          <a href="#">The Number Ones: Streisdan & Diamond's "You Don't Bring Me Flowers" 363<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/Chic-Le-Freak.jpg" alt=""/>
+          <div className="story-num">04</div>
+          <a href="#">The Number Ones: Chic's "Le Freak" 341<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/shutupdude.jpg" alt=""/>
+          <div className="story-num">05</div>
+          <a href="#">Shut Up, Dude: The Week's Best & Worst Comments 193<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/grimes.jpg" alt=""/>
+          <div className="story-num">06</div>
+          <a href="#">Grimes Announces Pregnancy With Instagram Photo 178<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/contra10.jpg" alt=""/>
+          <div className="story-num">07</div>
+          <a href="#">Conta Turns 10 140<i className="far fa-comment"></i></a>
+        </div>
+
+        <div className="pop-story">
+          <img src="../images/Bright-Eyes.jpg" alt=""/>
+          <div className="story-num">08</div>
+          <a href="#">Bright Eyes Tease Comeback 134<i className="far fa-comment"></i></a>
+        </div>
+        */}
 
       </div>
       
