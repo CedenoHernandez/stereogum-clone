7b840c4577873642b2d82318ecfe5f215f32016e
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-14 16:14:48 -0800

    Add links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,22 @@ class Navbar extends Component {
           <a href="#">Lists</a>
           <a href="#">Videos</a>
         </div>  
+
         <div className="siteLogos">
-          <i className="fab fa-facebook-f fa-lg"></i>
-          <i className="fab fa-twitter fa-lg"></i>
-          <i className="fab fa-instagram fa-lg"></i>
-          <i className="fab fa-spotify fa-lg"></i>
+          <a href="https://www.facebook.com/Stereogum" target="_blank">
+            <i className="fab fa-facebook-f fa-lg"></i>
+          </a>
+          <a href="https://twitter.com/stereogum" target="_blank">
+            <i className="fab fa-twitter fa-lg"></i>
+          </a>
+          <a href="https://www.instagram.com/stereogum/" target="_blank">
+            <i className="fab fa-instagram fa-lg"></i>
+          </a>
+          <a href="https://open.spotify.com/user/stereogum" target="_blank">
+            <i className="fab fa-spotify fa-lg"></i>
+          </a>
         </div>
+
         <div className="search">
           <i className="fas fa-search fa-lg"></i>
         </div>
