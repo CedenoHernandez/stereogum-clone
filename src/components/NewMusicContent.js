af7682696473bdb90ede7b80378955c0ec6c1b4b
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-16 14:12:17 -0800

    Complete page with news content

--- src/components/NewMusicContent.js
+++ src/components/NewMusicContent.js
@@ -12,31 +12,15 @@ class NewMusicContent extends Component {
         
         <container className="stories">
           <div className="news-img">
-            <img src="../images/Gigaton.png" alt="" className="story-img"/>
+            <img src="../images/1975.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Pearl Jam Announces New Album <i>Gigaton</i></h2>
-            <p>Pearl Jam have been talking about a new album for what feels like forever, and last week they began teasing something called "Gigaton" via augmented…</p>
-            <div className="news-info-sm">
-              <a href="#" className="news-author">Chris DeVille</a>
-              <p className="news-date">| January 13, 2020 - 1:22 pm</p>
-              <p className="comment-count">62</p>
-              <i className="far fa-comment"></i>
-            </div>
-          </div>
-        </container>
-
-        <container className="stories">
-          <div className="news-img">
-            <img src="../images/us-girls-heavy-light-cover.jpg" alt="" className="story-img"/>
-          </div>
-          <div className="news-brief">
-            <h2>U.S. Girls - "Overtime"</h2>
-            <p>Just under two years ago, Meg Remy released In A Poem Unlimited, her sixth album as U.S. Girls. While the project had already found devout…</p>
+            <h2>The 1975 - "Me & You Together Song"</h2>
+            <p>The rollout for the 1975's forthcoming album Notes On A Conditional Form has been long. I mean, it's been really, really long if you consider…</p>
             <div className="news-info-sm">
               <a href="#" className="news-author">Ryan Leas</a>
-              <p className="news-date">| January 13, 2020 - 10:03 am</p>
-              <p className="comment-count">20</p>
+              <p className="news-date">| January 16, 2020 - 2:36 pm</p>
+              <p className="comment-count">57</p>
               <i className="far fa-comment"></i>
             </div>
           </div>
@@ -44,15 +28,47 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/dan-deacon.jpg" alt="" className="story-img"/>
+            <img src="../images/Green-Day-Oh-Yeah.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Dan Deacon - "Become a Mountain"</h2>
-            <p>At the end of the month, Dan Deacon is releasing a new album, Mystic Familiar, his first proper full-length album since 2015's Gliss Rifter after…</p>
+            <h2>Green Day - "Oh Yeah!"</h2>
+            <p>Is the world ready for a Gary Glitter renaissance? No! Absolutely not! That would be a terrible idea! And yet Glitter, the former glam-rock star…</p>
+            <div className="news-info-sm">
+              <a href="#" className="news-author">Tom Breihan</a>
+              <p className="news-date">| January 16, 2020 - 11:55 am</p>
+              <p className="comment-count">17</p>
+              <i className="far fa-comment"></i>
+            </div>
+          </div>
+        </container>
+
+        <container className="stories">
+          <div className="news-img">
+            <img src="../images/squirrel-flower.jpg" alt="" className="story-img"/>
+          </div>
+          <div className="news-brief">
+            <h2>Squirrel Flower - "Streetlight Blues"</h2>
+            <p>At the end of this month, Ella O’Connor Williams will release I Was Born Swimming, her debut album under the moniker Squirrel Flower. We've already…</p>
+            <div className="news-info-sm">
+              <a href="#" className="news-author">Ryan Leas</a>
+              <p className="news-date">| January 16, 2020 - 11:44 am</p>
+              <p className="comment-count">5</p>
+              <i className="far fa-comment"></i>
+            </div>
+          </div>
+        </container>
+
+        <container className="stories">
+          <div className="news-img">
+            <img src="../images/pom-pom.jpg" alt="" className="story-img"/>
+          </div>
+          <div className="news-brief">
+            <h2>Pom Pom Squad - "Cellophane" (FKA Twigs Cover)</h2>
+            <p>Pom Pom Squad released a pretty promising EP, Ow, last year, and they're starting off 2020 with a cover of one of their favorite songs…</p>
             <div className="news-info-sm">
               <a href="#" className="news-author">James Rettig</a>
-              <p className="news-date">| January 13, 2020 - 10:00 am</p>
-              <p className="comment-count">15</p>
+              <p className="news-date">| January 16, 2020 - 10:24 am</p>
+              <p className="comment-count">2</p>
               <i className="far fa-comment"></i>
             </div>
           </div>
@@ -60,30 +76,14 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/gordi-the-cost.jpg" alt="" className="story-img"/>
+            <img src="../images/memories.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Gordi - "The Cost"</h2>
-            <p>The Australian Artist To Watch Gordi -- real name Sophie Payten -- released her debut album back in 2017, and today she's returned with a…</p>
-            <div className="news-info-sm">
-              <a href="#" className="news-author">James Rettig</a>
-              <p className="news-date">| January 12, 2020 - 11:39 am</p>
-              <p className="comment-count"></p>
-              <i className="far fa-comment"></i>
-            </div>
-          </div>
-        </container>
-
-        <container className="stories">
-          <div className="news-img">
-            <img src="../images/rush-drummer.jpg" alt="" className="story-img"/>
-          </div>
-          <div className="news-brief">
-            <h2>Dave Grohl Shares Eulogy For Neil Peart</h2>
-            <p>Rush drummer Neil Peart passed away this week due to complications from brain cancer. And now another famous drummer, Dave Grohl of Nirvana and Foo…</p>
+            <h2>Swamp Dogg - "Memories" (Feat. John Prine & Justin Vernon)</h2>
+            <p>Six decades into his career, outsider soul legend Swamp Dogg is still taking risks. He reinvented himself with his last studio album, 2018's Love, Loss,…</p>
             <div className="news-info-sm">
               <a href="#" className="news-author">Peter Helman</a>
-              <p className="news-date">| January 11, 2020 - 10:43 am</p>
+              <p className="news-date">| January 16, 2020 - 10:21 am</p>
               <p className="comment-count">4</p>
               <i className="far fa-comment"></i>
             </div>
@@ -92,15 +92,15 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/mac-miller.jpg" alt="" className="story-img"/>
+            <img src="../images/Kyle-Forester.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Mac Miller - "Good News"</h2>
-            <p>Yesterday, Mac Miller's family announced that a posthumous album from the musician was on the way. Circles is due out next Friday (1/17), and a…</p>
+            <h2>Kyle Forester - "Know What You're Doing"</h2>
+            <p>Kyle Forester is one of those Indie Rock Dudes who's had a hand in a bunch of cool shit. He was a member of Crystal…</p>
             <div className="news-info-sm">
-              <a href="#" className="news-author">James Rettig</a>
-              <p className="news-date">| January 9, 2020 - 6:12 pm</p>
-              <p className="comment-count">21</p>
+              <a href="#" className="news-author">Chris Deville</a>
+              <p className="news-date">| January 16, 2020 - 10:18 am</p>
+              <p className="comment-count">2</p>
               <i className="far fa-comment"></i>
             </div>
           </div>
@@ -108,15 +108,31 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/abbey-road.jpg" alt="" className="story-img"/>
+            <img src="../images/Margo-Price.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Abbey Road Was the Best-Selling Vinyl of the Decade</h2>
-            <p>People are still coming together right now over the Beatles. As 2019 drew to a close, so did the entire decade, and according to Nielsen…</p>
+            <h2>Margo Price - "Stone Me"</h2>
+            <p>It's been more than two years since the sharp, incisive old-school country badass Margo Price came out with All American Made, her last album. In…</p>
+            <div className="news-info-sm">
+              <a href="#" className="news-author">Tom Breihan</a>
+              <p className="news-date">| January 16, 2020 - 8:53 am</p>
+              <p className="comment-count">6</p>
+              <i className="far fa-comment"></i>
+            </div>
+          </div>
+        </container>
+
+        <container className="stories">
+          <div className="news-img">
+            <img src="../images/thundercat.jpg" alt="" className="story-img"/>
+          </div>
+          <div className="news-brief">
+            <h2>Thundercat - "Black Qualls" (Feat. Steve Lacy & Steve Arrington)</h2>
+            <p>Thundercat always stays busy, but the Los Angeles bass wizard hasn't released an album of his own since 2017's Drunk. Fortunately, that's about to change.</p>
             <div className="news-info-sm">
               <a href="#" className="news-author">Peter Helman</a>
-              <p className="news-date">| January 9, 2020 - 5:52 pm</p>
-              <p className="comment-count">79</p>
+              <p className="news-date">| January 15, 2020 - 1:33 pm</p>
+              <p className="comment-count">32</p>
               <i className="far fa-comment"></i>
             </div>
           </div>
@@ -124,30 +140,14 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/Selena-gomez.jpg" alt="" className="story-img"/>
+            <img src="../images/End-It.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Selena Gomez's <i>Rare</i> Was Worth The Wait</h2>
-            <p>Rare is an accurate title for a Selena Gomez album. Gomez, the former Disney Channel teen actress turned pop superstar and tabloid fixation, is part…</p>
+            <h2>Stream End It's <i>One Way Track</i> EP</h2>
+            <p>The Baltimore band End It play fast, bruising, ignorant hardcore -- absolute fight music with no room for nuance or subtlety. Back in 2017, End…</p>
             <div className="news-info-sm">
-              <a href="#" className="news-author">Chris DeVille</a>
-              <p className="news-date">| January 9, 2020 - 4:00 pm</p>
-              <p className="comment-count">45</p>
-              <i className="far fa-comment"></i>
-            </div>
-          </div>
-        </container>
-
-        <container className="stories">
-          <div className="news-img">
-            <img src="../images/Bright-Eyes.jpg" alt="" className="story-img"/>
-          </div>
-          <div className="news-brief">
-            <h2>Bright Eyes Tease Comeback</h2>
-            <p>Bright Eyes are back. Conor Oberst's massively popular and influential indie rock/folk/emo project has been inactive since touring behind 2011 swan song The People's Key.</p>
-            <div className="news-info-sm">
-              <a href="#" className="news-author">Chris DeVille</a>
-              <p className="news-date">| January 9, 2020 - 2:28 pm</p>
+              <a href="#" className="news-author">Tom Breihan</a>
+              <p className="news-date">| January 15, 2020 - 12:48 pm</p>
               <p className="comment-count">134</p>
               <i className="far fa-comment"></i>
             </div>
@@ -156,14 +156,14 @@ class NewMusicContent extends Component {
 
         <container className="stories">
           <div className="news-img">
-            <img src="../images/Black-Lips.png" alt="" className="story-img"/>
+            <img src="../images/loose-buttons.jpg" alt="" className="story-img"/>
           </div>
           <div className="news-brief">
-            <h2>Black Lips - "Rumbler"</h2>
-            <p>Black Lips sound revitalized on the singles from their imminent Black Lips Sing In A World That’s Falling Apart, "Odelia" and especially "Gentleman." But they're…</p>
+            <h2>Loose Buttons - "I Don't Really Know"</h2>
+            <p>The poppy New York garage rockers Loose Buttons have been rolling out some extremely pleasing music from their forthcoming debut album. Something Better has thus…</p>
             <div className="news-info-sm">
               <a href="#" className="news-author">Chris DeVille</a>
-              <p className="news-date">| January 9, 2020 - 12:56 pm</p>
+              <p className="news-date">| January 5, 2020 - 12:42 pm</p>
               <p className="comment-count">5</p>
               <i className="far fa-comment"></i>
             </div>
